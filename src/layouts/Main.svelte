91c36dc847306029bc9729b8517ff3c5360572ff
Alvi Hysa <alvihysa89@gmail.com>
2023-01-15 19:39:16 +0100

    Initial Commit

---
+++ src/layouts/Main.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  export let title: string = "Website Title.";
+</script>
+
+<main>
+  <h1 class="text-[18rem] font-sans font-black text-slate-900">{title}</h1>
+</main>
+
+<style>
+</style>
