79fbe6a84d7cc95b416c5a65b909b71198af6248
alvi <38663469+alvinosh@users.noreply.github.com>
2023-02-07 10:30:38 +0100

    Added Planets

--- src/content/planets.js
+++ src/content/planets.js
@@ -23,6 +23,46 @@ export default `
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus.",
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus."
     ]
+  },
+  {
+    "title": "Mars",
+    "header": "Information",
+    "text": [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus."
+    ]
+  },
+  {
+    "title": "Jupiter",
+    "header": "Information",
+    "text": [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus."
+    ]
+  },
+  {
+    "title": "Saturn",
+    "header": "Information",
+    "text": [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus."
+    ]
+  },
+  {
+    "title": "Uranus",
+    "header": "Information",
+    "text": [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus."
+    ]
+  },
+  {
+    "title": "Neptune",
+    "header": "Information",
+    "text": [
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, neque! Earum veniam id ipsam. Quod, mollitia! Rem labore dolorem libero, magnam molestiae sapiente aliquid placeat commodi numquam laboriosam ratione temporibus."
+    ]
   }
 ]
 `;
